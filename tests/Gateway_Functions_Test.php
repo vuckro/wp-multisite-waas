6ff9a284037bb176adee8b609fc294ac30415e1c
David Stone <david@nnucomputerwhiz.com>
2025-02-01 15:18:13 -0700

    Setup code quality and tests

---
+++ tests/Gateway_Functions_Test.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace WP_Ultimo\Tests;
+
+use WP_Ultimo\Gateways\Manual_Gateway;
+use WP_Ultimo\Managers\Gateway_Manager;
+
+class Gateway_Functions_Test extends \WP_UnitTestCase {
+
+	public function test_wu_get_gateway_returns_false_for_invalid_id() {
+	    $invalid_gateway_id = 'non_existent_gateway';
+
+	    $result = wu_get_gateway($invalid_gateway_id);
+
+	    $this->assertFalse($result);
+	}
+
+	public function test_wu_get_gateway_returns_instance_for_valid_id() {
+	    $valid_gateway_id = 'manual';
+
+	    $gateway = wu_get_gateway($valid_gateway_id);
+
+	    $this->assertInstanceOf(Manual_Gateway::class, $gateway);
+	}
+
+}
---
+++ tests/WP_Ultimo_Test.php
@@ -0,0 +1,36 @@
+<?php
+namespace WP_Ultimo;
+
+class WP_Ultimo_Test extends \WP_UnitTestCase {
+    /**
+     * Test if all helper functions are loaded correctly.
+     *
+     * This test case creates an instance of the WP_Ultimo class and calls the load_public_apis method.
+     * It then asserts that the helper functions wu_to_float, wu_replace_dashes, and wu_get_initials are
+     * correctly loaded. Additional assertions can be added for other helper functions.
+     *
+     * @return void
+     */
+    public function testLoadAllHelperFunctionsCorrectly() {
+        // Assert that all helper functions are loaded correctly.
+	    // This is all done in the bootstrap.
+        $this->assertTrue(function_exists('wu_to_float'));
+        $this->assertTrue(function_exists('wu_replace_dashes'));
+        $this->assertTrue(function_exists('wu_get_initials'));
+    }
+
+	public function testLoaded() {
+		$wpUltimo = \WP_Ultimo();
+		$this->assertTrue($wpUltimo->version === \WP_Ultimo::VERSION);
+		$this->assertTrue($wpUltimo->is_loaded());
+	}
+
+	public function testPublicProperties() {
+		$wpUltimo = \WP_Ultimo();
+        $this->assertTrue($wpUltimo->settings instanceof Settings);
+        $this->assertTrue($wpUltimo->helper instanceof Helper);
+		$this->assertTrue($wpUltimo->notices instanceof Admin_Notices);
+		$this->assertTrue($wpUltimo->scripts instanceof Scripts);
+		$this->assertTrue($wpUltimo->currents instanceof Current);
+	}
+}
