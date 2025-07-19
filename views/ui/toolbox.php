5a0430954998d793018349546c7409790e088a11
David Stone <david@nnucomputerwhiz.com>
2025-07-19 17:52:16 -0600

    don't escape too much

--- views/ui/toolbox.php
+++ views/ui/toolbox.php
@@ -63,7 +63,7 @@ defined( 'ABSPATH' ) || exit;
 				class="dashicons-wu-circular-graph wu-text-sm wu-w-auto wu-h-auto wu-align-text-bottom wu-relative"></span>
 			<span class="">
 				<?php // translators: %s hash of membership. ?>
-				<?php printf(esc_html__('Membership <Strong>%s</strong>', 'multisite-ultimate'), esc_html($membership->get_hash())); ?>
+				<?php printf(wp_kses_post('Membership <Strong>%s</strong>', 'multisite-ultimate'), esc_html($membership->get_hash())); ?>
 			</span>
 			<span id="wu-toolbox-membership-status" class="wu-inline-block wu-w-3 wu-h-3 wu-rounded-full wu-align-text-top <?php echo esc_attr($membership->get_status_class()); ?>" <?php echo wu_tooltip_text($membership->get_status_label()); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped ?>>
 				&nbsp;
