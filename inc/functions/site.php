3f01398341a43065d6cbdf21c52ec15aea25ac3f
vuckro <vvwebbusiness@gmail.com>
2025-08-08 19:42:43 +0200

    Update site.php

--- inc/functions/site.php
+++ inc/functions/site.php
@@ -568,5 +568,5 @@ function wu_add_site_meta_options_section($sections, $site) {
 	return $sections;
 }
 
-// Hook into site options sections - Priority 12 to place after Reset Limitations but before Notes
-add_filter('wu_site_options_sections', 'wu_add_site_meta_options_section', 12, 2);
+// Hook into site options sections - Priority 20 to place at the bottom of the list
+add_filter('wu_site_options_sections', 'wu_add_site_meta_options_section', 20, 2);
